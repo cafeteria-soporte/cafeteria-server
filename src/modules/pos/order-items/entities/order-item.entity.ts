f5b2e5e229cff600c8a761552b2a1c312abcc4ce
Sergini34 <mendietasergio83gmail.com>
2026-05-22 17:26:32 -0400

    cambios en product-service

--- src/modules/pos/order-items/entities/order-item.entity.ts
+++ src/modules/pos/order-items/entities/order-item.entity.ts
@@ -4,26 +4,26 @@ import { Product } from 'src/modules/product-catalog/products/entities/product.e
 
 @Entity('order_items')
 export class OrderItem {
-    @PrimaryColumn({ name: 'user_order_id', type: 'int' })
-    userOrderId: number;
+  @PrimaryColumn({ name: 'user_order_id', type: 'int' })
+  userOrderId: number;
 
-    @PrimaryColumn({ name: 'product_id', type: 'int' })
-    productId: number;
+  @PrimaryColumn({ name: 'product_id', type: 'int' })
+  productId: number;
 
-    @Column({ name: 'quantity', type: 'int' })
-    quantity: number;
+  @Column({ name: 'quantity', type: 'int' })
+  quantity: number;
 
-    @Column({ name: 'unit_price', type: 'decimal', precision: 10, scale: 2 })
-    unitPrice: number;
+  @Column({ name: 'unit_price', type: 'decimal', precision: 10, scale: 2 })
+  unitPrice: number;
 
-    @Column({ name: 'subtotal', type: 'decimal', precision: 10, scale: 2 })
-    subtotal: number;
+  @Column({ name: 'subtotal', type: 'decimal', precision: 10, scale: 2 })
+  subtotal: number;
 
-    @ManyToOne(() => UserOrder, (o) => o.items)
-    @JoinColumn({ name: 'user_order_id' })
-    userOrder?: UserOrder;
+  @ManyToOne(() => UserOrder, (o) => o.items)
+  @JoinColumn({ name: 'user_order_id' })
+  userOrder?: UserOrder;
 
-    @ManyToOne(() => Product)
-    @JoinColumn({ name: 'product_id' })
-    product?: Product;
+  @ManyToOne(() => Product)
+  @JoinColumn({ name: 'product_id' })
+  product?: Product;
 }
